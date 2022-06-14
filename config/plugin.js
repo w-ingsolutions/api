aca7e59eb6920f1797643b0946a1a21a6f723e7e
marcetin <marcetin@gmail.com>
2022-06-14 04:30:42 +0200

    dag

--- config/plugin.js
+++ config/plugin.js
@@ -1,4 +1,19 @@
 module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('marcetin'),
+        api_key: env('598282915278376'),
+        api_secret: env('598282915278376'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
   'cloudflare-pages': {
     instances: [
       {
