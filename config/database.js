cf962502ef7d24fd360a22136aad9efa2bc5953c
marcetin <marcetin@gmail.com>
2022-06-14 03:48:25 +0200

    da

--- config/database.js
+++ config/database.js
@@ -1,11 +1,17 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'eu-central-1.22f36672-2673-4092-8ef8-a4484274a9e4.aws.ybdb.io'),
+      port: env.int('DATABASE_PORT', 5433),
+      database: env('DATABASE_NAME', 'yugabyte'),
+      user: env('DATABASE_USERNAME', 'strapiuser'),
+      password: env('DATABASE_PASSWORD', '2vc2BLL4ZiFwRy2'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
